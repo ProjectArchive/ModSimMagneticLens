4b2067fd45f4a2d184d4f1f7a705b360397c9017
nrubin <noam.rubin@students.olin.edu>
2011-03-06 17:59:44 -0500

    working TwoRing code

--- CurrentWork/twoRings.m
+++ CurrentWork/twoRings.m
@@ -1,9 +1,11 @@
 function res = twoRings()
 mu_0 = 1e-7;
-M = [0;0;-1.5];
+magVec = [0;0;-1.5];
 rad = .05;
 q = -1.6e-19;
 m=9.1e-31;
+d = .5;
+particleLaunch()
 
     function res = particleLaunch()
         Rx = 0;
@@ -13,12 +15,13 @@ m=9.1e-31;
         Vy = 0;
         Vz = -1e8;
         hold on
-        LaunchAnElectron([Rx;Ry;Rz],[Vx;Vy;Vz])
-        LaunchAnElectron([Rx;Ry;Rz],[-Vx;Vy;Vz])
+        M = LaunchAnElectron([Rx;Ry;Rz],[Vx;Vy;Vz]);
+        plot3(M(:,1),M(:,2),M(:,3))
         theta = linspace(0,2*pi,2000);
         A = cos(theta);
         B = sin(theta);
         plot3(rad*A,rad*B,zeros(1,length(A)),'r','LineWidth',5);
+        plot3(rad*A,rad*B,-d*ones(1,length(A)),'k','LineWidth',5);
         xlabel('X(m)');
         ylabel('Y(m)');
         zlabel('Z(m)');
@@ -31,7 +34,7 @@ m=9.1e-31;
         end
         options = odeset('Events',@events);
         [T, M] = ode45(@projectile, [0,.0000001], [R_init,V_init],options);
-        plot3(M(:,1),M(:,2),M(:,3));
+        res = M;
     end
     function res = projectile(t, W)
         R = W(1:3);
@@ -52,12 +55,13 @@ m=9.1e-31;
     end
     function iRes = theIntegrand(rMeasure,theta)
         rSource1 = [rad*cos(theta);rad*sin(theta);0]; % parametize the source curve of dipoles
-        iRes = magneticFieldOfR(rMeasure,rSource1);
+        rSource2 = [rad*cos(theta);rad*sin(theta);-d];
+        iRes = magneticFieldOfR(rMeasure,rSource1) + magneticFieldOfR(rMeasure,rSource2);
     end
     function magField = magneticFieldOfR(rMeasure,rSource)
         displacement =(rMeasure-rSource);
         coefficients = mu_0/(norm(displacement)^3);
         D_hat = (displacement)/norm(displacement);
-        magField = coefficients*(3*(dot(M,D_hat))*D_hat - M);
+        magField = coefficients*(3*(dot(magVec,D_hat))*D_hat - magVec);
     end
 end
