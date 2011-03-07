0af7823704ff7704e70aee0679871b9ec8fb66ed
wdolphin <wcdolphin@gmail.com>
2011-03-07 14:45:27 -0500

    changed getFocalLength, adding a smarter algorithm

--- CurrentWork/getFocalLength.m
+++ CurrentWork/getFocalLength.m
@@ -2,15 +2,30 @@ function focalLength = getFocalLength(MVector)
 X = MVector(:,1);
 Y = MVector(:,2);
 Z = MVector(:,3);
-maxError = 0.0000001;
+relTol = 0.000001;
 
-while maxError < 0.1 %while we have not found the focal length or else the error is reasonable
-    for i=cast(length(X)/4,'uint8'):length(X) %iterate through all of the X values
-        if abs(X(i)-X(1)) < maxError % test if this X value is close to the center line
-           focalLength= Z(i); %return the height 
-           return
+zStart = findZeroZIndex(Z);
+while relTol < 1 %while we have not found the focal length or else the error is reasonable
+    for i=zStart:length(X) %iterate through all of the X values after the Z plane
+        if norm([X(i),Y(i)]) < relTol % test if this X value is close to the center line
+            focalLength= Z(i); %return the height
+            return
         end
     end
-    maxError = maxError * 1.5;
+    relTol = relTol * 1.5;
+end
+    
+    function findZero = findZeroZIndex(zVector)
+        zIndexStart = 0;
+        curDist = zVector(1);
+        for zIndex = 1:length(zVector)
+            if abs(0 -zVector(zIndex)) < curDist %this is closer than our closest
+                curDist = abs(0 -zVector(zIndex));
+                zIndexStart = zIndex;
+            end
+        end
+        findZero = zIndexStart;
+    end
+
 end
 
