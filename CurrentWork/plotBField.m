9c6044ff34f26704d34a6d322bb8501e637f79fb
wdolphin <wcdolphin@gmail.com>
2011-03-09 18:25:25 -0500

    added magnetic visualization plot

--- CurrentWork/plotBField.m
+++ CurrentWork/plotBField.m
@@ -6,25 +6,25 @@ rad = .05;
 I = 50;
 
 % theta = linspace(0,2*pi,20);
-x = linspace(-.075,.075,20);
-y = linspace(-.075,.075,20);
+x = linspace(-.075,.075,200);
+y = linspace(-.075,.075,200);
 z = linspace(-.5,.5,20);
 
-[X Y Z] = meshgrid(x,y,z);
+[X Y ] = meshgrid(x,y);
 
 for i_x =1:length(x)
     for i_y =1:length(y)
-        for i_z =1:length(z)
-            measVec = [x(i_x);y(i_y);z(i_z)];
+       % for i_z =1:length(z)
+            measVec = [x(i_x);y(i_y);0.0005];
             BField = MagneticFieldAtAPoint(measVec);
-            U(i_x,i_y,i_z) = BField(1);
-            V(i_x,i_y,i_z) = BField(2);
-            W(i_x,i_y,i_z) = BField(3);
+            U(i_x,i_y) = BField(1);
+            V(i_x,i_y) = BField(2);
+            %W(i_x,i_y,i_z) = BField(3);
         end
-    end
 end
-startPoint = [0,0,0];
-streamslice(X,Y,Z,U,V,W,0,0,0)
+streamslice(X,Y,U,V)
+hold on;
+
 
     function res = MagneticFieldAtAPoint(rMeasure)
         
