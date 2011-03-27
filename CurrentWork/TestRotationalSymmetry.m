da686ed586480e92be5d04d66e785bcf5906f86f
wcdolphin <wcdolphin@gmail.com>
2011-03-27 17:44:37 -0400

    added new figures section, changed focal length

--- CurrentWork/TestRotationalSymmetry.m
+++ CurrentWork/TestRotationalSymmetry.m
@@ -12,27 +12,12 @@ plotRing(ringRadius)
 hold on
 
 indices = 1;
-display(indices);
 temp = LaunchAnElectron([xParticle,yParticle,zParticle],[0,0,-1e7],ringRadius,50);
 thisRun = temp(:,1:3);
-plot3(thisRun(:,1),thisRun(:,2),thisRun(:,3))
-
+plot3(thisRun(:,1),thisRun(:,2),thisRun(:,3));
+theFocalLength = getFocalLength(thisRun);
 display(thisRun(1,:));
-
 numParticles = 20;
 rTheta=linspace(0,2*pi,numParticles);
-theSeparation = [];
-for(index = 1 : length(thisRun(:,1)))
-    
-    for( rot = 1: length(rTheta))
-    %z values constant, rotate x and y about the line x,y = 0;
-        rotX =thisRun(index,1).*cos(rTheta(rot)) -thisRun(index,2).*sin(rTheta(rot));
-        rotY =thisRun(index,1).*sin(rTheta(rot)) +thisRun(index,2).*cos(rTheta(rot));
-        rotZ = thisRun(index,3);
-        plot3(rotX,rotY,rotZ);
-        theSeparation(index) = norm(rotX,rotY);
-    end
-end
 figure(2);
-plot(thisRun(:,3),theSeparation);
-
+%plot(thisRun(:,3),theFocalLength);
