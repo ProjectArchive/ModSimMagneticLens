a0311418b8cc77531fab9d2845163bb303b240da
nrubin <noam.rubin@students.olin.edu>
2011-03-27 16:03:22 -0400

    added current as a variable

--- CurrentWork/MultiParticleLauncher.m
+++ CurrentWork/MultiParticleLauncher.m
@@ -1,4 +1,4 @@
-function res = MultiParticleLauncher(numParticles,ringRadius,particleLaunchHeight,particleLaunchVelocity)
+function res = MultiParticleLauncher(numParticles,ringRadius,particleLaunchHeight,particleLaunchVelocity,current)
 
 
 theta = linspace(0,2*pi,numParticles);
@@ -8,23 +8,17 @@ xParticle = particleLaunchRadius*cos(theta);
 yParticle = particleLaunchRadius*sin(theta);
 zParticle = particleLaunchHeight;
 
-% plotRing(ringRadius)
-% hold on
+plotRing(ringRadius)
+hold on
 
-%focalLength = zeros(numParticles);
-%separation = zeros(numParticles);
+
 focalLengthAndSeparation= zeros(numParticles,2);
 for i = 1:numParticles
-    temp = LaunchAnElectron([xParticle(i),yParticle(i),zParticle],[0,0,-particleLaunchVelocity],ringRadius,50);
-%     plot3(temp(:,1),temp(:,2),temp(:,3))
+    temp = LaunchAnElectron([xParticle(i),yParticle(i),zParticle],[0,0,-particleLaunchVelocity],ringRadius,current);
+    plot3(temp(:,1),temp(:,2),temp(:,3))
     focalLengthAndSeparation(i,:) = getFocalLength(temp);
-    %display(focalLengthAndSeparation(:,2));
-    %focalLength(i) = focalLengthAndSeparation(1);
-    %separation(i) = focalLengthAndSeparation(2);
 end
 f = -(focalLengthAndSeparation(:,1));
-%display(f);
 s= -(focalLengthAndSeparation(:,2));
-%display(s);
 res = [f(1),s(1)];
 end
