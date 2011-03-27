ad99d8fabad24c2ee323dc8e7f53a98d23d05d32
wcdolphin <wcdolphin@gmail.com>
2011-03-27 19:01:50 -0400

    changed name

---
+++ CurrentWork/varyRingRadius.m
@@ -0,0 +1,22 @@
+%lolwut Focal Length
+clf;
+clc;
+clear all;
+radii = linspace(.01,.4,50);
+% velocities = logspace(1e5,1e8,5);
+for i = 1:length(radii)
+%     for j = 1:length(velocities)
+%         temp = MultiParticleLauncher(5,radii(i),.5,velocities(j));
+        temp = MultiParticleLauncher(2,radii(i),.5,1e7);
+        focalLengths(i) = temp(1);
+        separation(i) = temp(2);
+%     end
+end
+subplot(2,1,1)
+plot(radii,focalLengths)
+xlabel('radius')
+ylabel('Focal Length')
+subplot(2,1,2)
+plot(radii,separation)
+xlabel('radius')
+ylabel('separation')
